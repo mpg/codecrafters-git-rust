43e2b49834f3a4870f5d6a6df03728022ee713e1
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2025-01-18 10:48:51 +0100

    Refactor: error handling with anyhow

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,31 @@
+use anyhow::{Context, Result};
 use std::env;
 use std::fs;
 
-fn git_init() {
-    fs::create_dir(".git").unwrap();
-    fs::create_dir(".git/objects").unwrap();
-    fs::create_dir(".git/refs").unwrap();
-    fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
+fn mkdir(path: &str) -> Result<()> {
+    fs::create_dir(path).with_context(|| format!("Could not create directory `{}`", path))
 }
 
-fn main() {
+fn write_file(path: &str, content: &[u8]) -> Result<()> {
+    fs::write(path, content).with_context(|| format!("Could not write to file `{}`", path))
+}
+
+fn git_init() -> Result<()> {
+    mkdir(".git")?;
+    mkdir(".git/objects")?;
+    mkdir(".git/refs")?;
+    write_file(".git/HEAD", b"ref: refs/heads/main\n")?;
+    Ok(())
+}
+
+fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
-        git_init();
+        git_init()?;
         println!("Initialized git directory");
     } else {
         println!("Unknown command: {}", args[1]);
     }
+
+    Ok(())
 }
