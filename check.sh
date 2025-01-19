4f6adc34a2b40ad74a424fd22d63416099ddb373
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2025-01-19 11:19:53 +0100

    git init <path>

---
+++ check.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+
+set -eu
+
+cargo build
+cargo fmt --check
+cargo clippy -- -D warnings
+
+ROOT="$PWD"
+TARGET="$ROOT/target/debug/codecrafters-git"
+TESTDIR="$ROOT/test-$$"
+cleanup() {
+    cd "$ROOT"
+    rm -rf "$TESTDIR"
+}
+setup() {
+    echo "$1"
+    mkdir "$TESTDIR"
+    cd "$TESTDIR"
+}
+trap cleanup EXIT
+cleanup
+
+assert_init() {
+    (
+        cd "$1"
+        test -d .git
+        test -d .git/objects
+        test -d .git/refs
+        grep -q '^ref: refs/heads/main' .git/HEAD
+    )
+}
+
+setup "git init"
+"$TARGET" init > mine
+assert_init "."
+rm -rf .git
+git init > ref
+diff mine ref
+cleanup
+
+setup "git init <path>"
+"$TARGET" init foo/bar > mine
+assert_init foo/bar
+rm -rf foo
+git init foo/bar > ref
+diff mine ref
+cleanup
--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,40 @@ fn mkdir(path: &str) -> Result<()> {
     fs::create_dir(path).with_context(|| format!("Could not create directory `{}`", path))
 }
 
+fn mkdir_p(path: &str) -> Result<()> {
+    fs::create_dir_all(path).with_context(|| format!("Could not create directory `{}`", path))
+}
+
 fn write_file(path: &str, content: &[u8]) -> Result<()> {
     fs::write(path, content).with_context(|| format!("Could not write to file `{}`", path))
 }
 
-fn git_init() -> Result<()> {
+fn git_init(path: &str) -> Result<()> {
+    mkdir_p(path)?;
+    let cwd = env::current_dir()?;
+    env::set_current_dir(path)?;
     mkdir(".git")?;
     mkdir(".git/objects")?;
     mkdir(".git/refs")?;
     write_file(".git/HEAD", b"ref: refs/heads/main\n")?;
+    env::set_current_dir(cwd)?;
     Ok(())
 }
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().collect();
     if args[1] == "init" {
-        git_init()?;
-        println!("Initialized git directory");
+        let path = if args.len() == 3 {
+            args[2].to_owned()
+        } else {
+            ".".to_string()
+        };
+        git_init(&path)?;
+        let canon = fs::canonicalize(path)?;
+        println!(
+            "Initialized empty Git repository in {}/.git/",
+            canon.display()
+        );
     } else {
         println!("Unknown command: {}", args[1]);
     }
