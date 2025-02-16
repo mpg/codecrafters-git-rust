de856dcf111af2300aa440d9acf5aa0678f0c34a
Manuel Pégourié-Gonnard <mpg@elzevir.fr>
2025-02-16 15:33:25 +0100

    Scaffolding for ref_delta objects

--- src/unpack.rs
+++ src/unpack.rs
@@ -4,6 +4,7 @@ use sha1::{Digest, Sha1};
 use std::io;
 use std::io::prelude::*;
 
+use crate::obj_read::ObjReader;
 use crate::obj_type::ObjType;
 use crate::obj_write::ObjWriter;
 
@@ -58,6 +59,56 @@ impl<R: Read> Read for HashingReader<R> {
     }
 }
 
+enum DeltaType {
+    OfsDelta,
+    RefDelta,
+}
+
+enum PackObjType {
+    Basic(ObjType),
+    Delta(DeltaType),
+}
+use PackObjType::*;
+
+impl PackObjType {
+    fn from_byte(value: u8) -> Result<Self> {
+        match value {
+            1 => Ok(Basic(ObjType::Commit)),
+            2 => Ok(Basic(ObjType::Tree)),
+            3 => Ok(Basic(ObjType::Blob)),
+            4 => Ok(Basic(ObjType::Tag)),
+            6 => Ok(Delta(DeltaType::OfsDelta)),
+            7 => Ok(Delta(DeltaType::RefDelta)),
+            _ => bail!("unknow pack object type: {}", value),
+        }
+    }
+}
+
+fn unpack_undeltified(reader: &mut impl BufRead, obj_type: ObjType, size: usize) -> Result<()> {
+    let mut zdec = ZlibDecoder::new(reader);
+    let mut object = ObjWriter::new(obj_type, size, true).context("creating object")?;
+    io::copy(&mut zdec, &mut object).context("copying data to object")?;
+    object
+        .finish()
+        .context("writing object to object database")?;
+    Ok(())
+}
+
+fn unpack_ref_delta(reader: &mut impl BufRead, size: usize) -> Result<()> {
+    let mut hash = [0u8; 20];
+    reader
+        .read_exact(&mut hash)
+        .context("reading base of ref_delta entry")?;
+    let hash = hex::encode(hash);
+    let base_obj =
+        ObjReader::from_hash(&hash).with_context(|| format!("opening base object {hash}"))?;
+    let writer = ObjWriter::new(base_obj.obj_type, size, true)
+        .context("creating new object from ref_delta")?;
+
+    let _ = writer;
+    bail!("ref_delta not implemented yet");
+}
+
 // gitformat-pack(5) "object entries, each of which looks like this"
 fn unpack_object(reader: &mut impl BufRead) -> Result<()> {
     // n-byte type and length (3-bit type, (n-1)*7+4-bit length)
@@ -66,13 +117,7 @@ fn unpack_object(reader: &mut impl BufRead) -> Result<()> {
         .read_exact(&mut buf)
         .context("reading object's first byte")?;
     let type_id = (buf[0] >> 4) & 0b111;
-    let obj_type = match type_id {
-        1 => ObjType::Commit,
-        2 => ObjType::Tree,
-        3 => ObjType::Blob,
-        4 => ObjType::Tag,
-        _ => bail!("deltified representation not supported yet"),
-    };
+    let pack_type = PackObjType::from_byte(type_id)?;
 
     // gitformat-pack(5) "Size encoding"
     let mut size = (buf[0] & 0x0f) as usize;
@@ -86,14 +131,11 @@ fn unpack_object(reader: &mut impl BufRead) -> Result<()> {
     }
 
     // compressed data
-    let mut zdec = ZlibDecoder::new(reader);
-    let mut object = ObjWriter::new(obj_type, size, true).context("creating object")?;
-    io::copy(&mut zdec, &mut object).context("copying data to object")?;
-    object
-        .finish()
-        .context("writing object to object database")?;
-
-    Ok(())
+    match pack_type {
+        Basic(obj_type) => unpack_undeltified(reader, obj_type, size),
+        Delta(DeltaType::RefDelta) => unpack_ref_delta(reader, size),
+        Delta(DeltaType::OfsDelta) => bail!("ofs_delta not supported"),
+    }
 }
 
 // gitformat-pack(5) "pack-*.pack files have the following format"
